1313f4e94f4d082f175b80617101dea9806ab4b8
heinthant <hthant00chk@gmail.com>
2023-04-25 19:09:37 +0630

    Create recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,32 @@ class RecipesController < ApplicationController
   def index
     @recipes = current_user.recipes.all
   end
+
+  def show 
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new 
+    @recipe = Recipe.new 
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    
+    if @recipe.save
+      flash[:success] = "Recipe created successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:error] = "An error occurred! Please try again."
+      render :new
+    end
+  end
+  
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
+  
 end
