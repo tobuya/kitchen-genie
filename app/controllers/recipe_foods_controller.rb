acfa38bcd4e3a26caab8dcd06b3a92037f3e9af7
heinthant <hthant00chk@gmail.com>
2023-04-26 00:48:26 +0630

    Implement adding ingredients

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,32 @@
+class RecipeFoodsController < ApplicationController
+    def new 
+        @foods = Food.all 
+        @recipe = Recipe.find(params[:recipe_id])
+    end
+
+    def create 
+        @recipe_food = RecipeFood.new(recipe_food_params)
+        @recipe = Recipe.find(params[:recipe_id])
+        @recipe_food.recipe = @recipe
+
+        if @recipe_food.save 
+            flash[:success] = "New ingredient added successfully!"
+            redirect_to recipe_path(@recipe)
+        end
+    end
+
+    def destroy 
+        @recipe_food = RecipeFood.find(params[:id])
+        @recipe_food.destroy
+
+        flash[:alert] = "Removed an ingredient!"
+        redirect_to recipe_path(@recipe_food.recipe)
+    end
+
+
+    private 
+
+    def recipe_food_params 
+        params.require(:recipe_food).permit(:food_id, :quantity)
+    end
+end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,7 @@ class RecipesController < ApplicationController
 
   def show 
     @recipe = Recipe.find(params[:id])
+    @ingredients = @recipe.recipe_foods
   end
 
   def new 
