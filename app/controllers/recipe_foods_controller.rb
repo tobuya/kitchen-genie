4ca31142359522ba3d03f6ee010483671c747174
heinthant <hthant00chk@gmail.com>
2023-04-26 01:13:29 +0630

    Format code

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,32 +1,32 @@
 class RecipeFoodsController < ApplicationController
-    def new 
-        @foods = Food.all 
-        @recipe = Recipe.find(params[:recipe_id])
-    end
+  def new
+    @foods = Food.all
+    @recipe = Recipe.find(params[:recipe_id])
+  end
 
-    def create 
-        @recipe_food = RecipeFood.new(recipe_food_params)
-        @recipe = Recipe.find(params[:recipe_id])
-        @recipe_food.recipe = @recipe
+  def create
+    @recipe_food = RecipeFood.new(recipe_food_params)
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food.recipe = @recipe
 
-        if @recipe_food.save 
-            flash[:success] = "New ingredient added successfully!"
-            redirect_to recipe_path(@recipe)
-        end
-    end
+    return unless @recipe_food.save
 
-    def destroy 
-        @recipe_food = RecipeFood.find(params[:id])
-        @recipe_food.destroy
+    flash[:success] = 'New ingredient added successfully!'
+    redirect_to recipe_path(@recipe)
+  end
 
-        flash[:alert] = "Removed an ingredient!"
-        redirect_to recipe_path(@recipe_food.recipe)
-    end
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe_food.destroy
+
+    flash[:alert] = 'Removed an ingredient!'
+    redirect_to recipe_path(@recipe_food.recipe)
+  end
 
 
-    private 
+  private
 
-    def recipe_food_params 
-        params.require(:recipe_food).permit(:food_id, :quantity)
-    end
+  def recipe_food_params
+    params.require(:recipe_food).permit(:food_id, :quantity)
+  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,49 +5,48 @@ class RecipesController < ApplicationController
     @recipes = current_user.recipes.all
   end
 
-  def show 
+  def show
     @recipe = Recipe.find(params[:id])
     @ingredients = @recipe.recipe_foods
   end
 
-  def new 
-    @recipe = Recipe.new 
+  def new
+    @recipe = Recipe.new
   end
 
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
-    
+
     if @recipe.save
-      flash[:success] = "Recipe created successfully!"
+      flash[:success] = 'Recipe created successfully!'
       redirect_to recipe_path(@recipe)
     else
-      flash[:error] = "An error occurred! Please try again."
+      flash[:error] = 'An error occurred! Please try again.'
       render :new
     end
   end
 
   def update
     @recipe = Recipe.find(params[:id])
-    if @recipe.update(recipe_params)
-      flash[:success] = "Updated visibility to #{@recipe.public ? "public" : "private"}!"
-      redirect_to recipe_path(@recipe)
-    end
+    return unless @recipe.update(recipe_params)
+
+    flash[:success] = "Updated visibility to #{@recipe.public ? 'public' : 'private'}!"
+    redirect_to recipe_path(@recipe)
   end
-  
-  def destroy 
+
+  def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
-    flash[:alert] = "Recipe: #{ @recipe.name.upcase } removed!"
+    flash[:alert] = "Recipe: #{@recipe.name.upcase} removed!"
 
     redirect_to recipes_path
   end
 
-  
+
   private
-  
+
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
-  
 end
