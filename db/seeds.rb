386ccce64d66e378b091a2a234a5ff6a755c2725
tobuya <thomasobuya95@gmail.com>
2023-04-25 11:13:48 +0300

    Add data food data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create(name: "John Doe")
+Food.create(name: "Apple", measurement_unit: 1, price: 50, quantity: 100, user_id: user.id)
+Food.create(name: "Orange", measurement_unit: 1, price: 75, quantity: 50, user_id: user.id)
+Food.create(name: "Banana", measurement_unit: 1, price: 30, quantity: 200, user_id: user.id)
+Food.create(name: "Potato", measurement_unit: 2, price: 20, quantity: 150, user_id: user.id)
