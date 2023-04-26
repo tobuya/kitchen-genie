3685cd04f70f330c91c1b40d364bb64c2c345ac3
tobuya <thomasobuya95@gmail.com>
2023-04-26 22:47:18 +0300

    Add assocaitions and validations tests

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,1 +1,27 @@
 require 'spec_helper'
+
+RSpec.describe Recipe, type: :model do
+  describe 'associations' do
+    let(:user) { User.create(name: 'John Doe') }
+    let(:recipe) { Recipe.new(user: user) }
+
+    it { should belong_to(:user) }
+    it { should have_many(:recipe_foods) }
+    it { should have_many(:foods).through(:recipe_foods) }
+  end
+
+  describe 'validations' do
+    let(:recipe) { Recipe.new }
+
+    it 'requires a name' do
+      expect(recipe).not_to be_valid
+      expect(recipe.errors[:name]).to include("can't be blank")
+    end
+
+    it 'requires a user' do
+      recipe.name = 'Chicken Tikka'
+      expect(recipe).not_to be_valid
+      expect(recipe.errors[:user]).to include('must exist')
+    end
+  end
+end
