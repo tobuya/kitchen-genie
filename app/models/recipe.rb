9f8843ece0fbe774b9e466c44799f40a10ce4cf1
tobuya <thomasobuya95@gmail.com>
2023-04-26 12:28:27 +0300

    Add count and total price methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,12 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods, source: :food
+
+  def food_count
+    foods.count
+  end
+
+  def food_total_price
+    foods.sum(:price)
+  end
 end
