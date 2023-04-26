5a27d6b5f25d4ed9f5aa9fee6b0de2d853950de2
tobuya <thomasobuya95@gmail.com>
2023-04-26 23:01:55 +0300

    Add associations and validations tests

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,1 +1,16 @@
 require 'spec_helper'
+
+RSpec.describe Food, type: :model do
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:recipe_foods) }
+    it { should have_many(:recipes).through(:recipe_foods).source(:recipe) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:measurement_unit) }
+    it { should validate_presence_of(:quantity) }
+  end
+end
