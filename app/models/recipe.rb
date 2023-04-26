a8e3f3a6597e46ca5fde5ae5738a84d1c3bf7bd3
heinthant <hthant00chk@gmail.com>
2023-04-26 16:00:36 +0630

    Add social links to footer

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods, source: :food
 
   def food_quantities_by_name 
