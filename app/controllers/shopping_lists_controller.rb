c503ddebfdeb287e21b32498f22a95f2a04b07ad
heinthant <hthant00chk@gmail.com>
2023-04-26 13:30:25 +0630

    Refine calculation of shopping list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,30 +1,33 @@
 class ShoppingListsController < ApplicationController 
-    def index 
-        # Select all recipes of current user
-        @recipes = Recipe.includes(:foods).where(user_id: current_user.id)
-        @shopping_list = {}
+  def index 
+      # Select all recipes of current user
+      @recipes = Recipe.includes(:foods).where(user_id: current_user.id)
+      @food_quantities_by_name = @recipes[0].food_quantities_by_name
+      @shopping_list = {}
 
-        @recipes.each do |recipe|
-            recipe.foods.each do |food|
-                # Initialize food data if not in shopping list yet
-                @shopping_list[food.id] ||= { name: food.name, quantity: 0, measurement_unit: food.measurement_unit, price: food.price, value: 0 }
-                
-                # Sum all the quantity from recipe
-                @shopping_list[food.id][:quantity] += recipe.recipe_foods.find_by(food_id: food.id).quantity
-            end 
-        end
-        # Select foods of current user
-        @foods = current_user.foods 
+      @recipes.each do |recipe|
+          recipe.foods.each do |food|
+              # Initialize food data if not in shopping list yet
+              @shopping_list[food.name] ||= { name: food.name, quantity: 0, measurement_unit: food.measurement_unit, price: food.price, value: 0 }
+              
+              # Sum all the quantity from recipe
+              @shopping_list[food.name][:quantity] += recipe.food_quantities_by_name[food.name]
+          end 
+      end
+      # Select foods of current user
+      @foods = current_user.foods 
 
-        # Reduce quantity to buy if already present
-        @shopping_list.each do |food_id, food|
-            own_food = @foods.find_by(id: food_id)
-            
-            if own_food.present?
-                food[:quantity] -= own_food.quantity
-            end
-            
-            food[:value] = food[:quantity] * food[:price]
-        end
-    end
+      # Reduce quantity to buy if already present
+      @shopping_list.each do |food_name, food|
+          own_food = @foods.find_by(name: food_name)
+          
+          food[:quantity] -= own_food.quantity if own_food
+          
+          
+          food[:value] = food[:quantity] * food[:price]
+      end
+
+      @food_types = @shopping_list.values.select {|i| i[:quantity] > 0 }.length
+      @total_value = @shopping_list.values.map {|i| i[:value]}.sum
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods, source: :food
+
+  def food_quantities_by_name 
+    recipe_foods.joins(:food).group('foods.name').sum(:quantity)
+  end
 end
